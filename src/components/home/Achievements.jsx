fea937cbc490dc9c511e204a9fb8e3b6210ef821
Bhatt, Vishal <vishalbhatt@eaton.com>
2020-10-13 17:43:49 +0530

    New Website First Cut

---
+++ src/components/home/Achievements.jsx
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Pdf from "../../editable-stuff/resume.pdf";
+import {
+  aboutHeading,
+  aboutDescription,
+  showInstaProfilePic,
+  instaLink,
+  instaUsername,
+  instaQuerry,
+} from "../../editable-stuff/configurations.json";
+
+const Achievements = () => {
+  const [instaProfilePic, setInstaProfilePic] = useState("");
+  const [showInsta, setShowInsta] = useState(showInstaProfilePic);
+  const [resumeURL] = useState(Pdf);
+
+  useEffect(() => {
+    if (showInsta) {
+      handleRequest();
+    }
+  }, [showInsta]);
+
+  const handleRequest = (e) => {
+    axios
+      .get(instaLink + instaUsername + instaQuerry)
+      .then((response) => {
+        // handle success
+        // console.log(response.data.graphql);
+        return setInstaProfilePic(
+          response.data.graphql.user.profile_pic_url_hd
+        );
+      })
+      .catch((error) => {
+        // handle error
+        setShowInsta(false);
+        return console.error(error.message);
+      })
+      .finally(() => {
+        // always executed
+      });
+  };
+
+  return (
+    <div id="achievements" className="jumbotron jumbotron-fluid m-0">
+      <div className="container container-fluid p-5">
+        <div className="row">
+          <h1>Achievements</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Achievements;
---
+++ src/components/home/Experience.jsx
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Pdf from "../../editable-stuff/resume.pdf";
+import {
+  aboutHeading,
+  aboutDescription,
+  showInstaProfilePic,
+  instaLink,
+  instaUsername,
+  instaQuerry,
+} from "../../editable-stuff/configurations.json";
+
+const Experience = () => {
+  const [instaProfilePic, setInstaProfilePic] = useState("");
+  const [showInsta, setShowInsta] = useState(showInstaProfilePic);
+  const [resumeURL] = useState(Pdf);
+
+  useEffect(() => {
+    if (showInsta) {
+      handleRequest();
+    }
+  }, [showInsta]);
+
+  const handleRequest = (e) => {
+    axios
+      .get(instaLink + instaUsername + instaQuerry)
+      .then((response) => {
+        // handle success
+        // console.log(response.data.graphql);
+        return setInstaProfilePic(
+          response.data.graphql.user.profile_pic_url_hd
+        );
+      })
+      .catch((error) => {
+        // handle error
+        setShowInsta(false);
+        return console.error(error.message);
+      })
+      .finally(() => {
+        // always executed
+      });
+  };
+
+  return (
+    <div id="experience" className="jumbotron jumbotron-fluid m-0">
+      <div className="container container-fluid p-5">
+        <div className="row">
+          <h1>Experience</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Experience;
